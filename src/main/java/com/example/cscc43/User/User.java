57061eb51263b61aab14fc3fd7a56cab6eb53e92
BaNg-W <bang344251871@gmail.com>
2024-07-17 12:01:11 -0400

    Create class user

---
+++ src/main/java/com/example/cscc43/User/User.java
@@ -0,0 +1,21 @@
+package com.example.cscc43.User;
+
+
+public class User{
+
+    private int user_id;
+    private String username;
+    private String email;
+    private String password_hash;
+
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
