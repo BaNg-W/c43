8959790c51c57e247a5fa8240ed62c6d83dcfbd3
BaNg-W <bang344251871@gmail.com>
2024-07-17 11:19:34 -0400

    Springboot init

---
+++ src/main/java/com/example/cscc43/Cscc43Application.java
@@ -0,0 +1,13 @@
+package com.example.cscc43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cscc43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cscc43Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cscc43/Cscc43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cscc43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cscc43ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
