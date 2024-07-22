38c351197116fd6901501ce2ddee16f471e62caa
a2003mc <a2003mc@gmail.com>
2024-07-22 15:12:59 -0400

    1
    1

--- src/main/java/cscc43/friend/FriendListRepo.java
+++ src/main/java/cscc43/friend/FriendListRepo.java
@@ -13,6 +13,5 @@ public interface FriendListRepo extends CrudRepository<FriendList, Integer> {
     List<FriendList> findByUserId(Integer userId);
 
     @Query(value = "DELETE FROM friend_list WHERE user_id = ?1 AND friend_id = ?2", nativeQuery = true)
-    void deleteFriend(Integer userId, Integer friendId);
     void deleteByUserIdAndFriendId(Integer userId, Integer friendId);
 }
